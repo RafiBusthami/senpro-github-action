aae9c427dcb9e3752f03c49cc30152fbb825a022
RafiBusthami <busthami.rafi@gmail.com>
2026-09-08 16:00:26 +0700

    anap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import { Inter } from '@next/font/google'
-// import styles from '@/styles/Home.module.css'
+ import Head from 'next/head'//ini
+ import Image from 'next/image'//adalah
+ import { Inter } from '@next/font/google'//komentar
+ import styles from '@/styles/Home.module.css'//ini 
 
 const inter = Inter({ subsets: ['latin'] })
 
